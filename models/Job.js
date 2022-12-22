46bf55bb9bde2dbdf236308edcd28090526ea373
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-12-22 19:49:35 +0530

    jobschema ready

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const mongoose =require('mongoose');
+
+
+const jobschema= new mongoose.Schema({
+    company:{
+        type:String,
+        required:[true,'Provide the company name'],
+        maxlength:50
+    },
+    position:{
+        type:String,
+        required:[true,'Provide the postion name'],
+        maxlength:10
+    },
+    status:{
+        type:String,
+        enum:['pending','interview','declined'],
+        default:'pending'
+    }
+    ,createdby:{
+        type:mongoose.Types.ObjectId,
+        required:[true,'Provide the user of the job'],
+        ref:'User'
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model('jobs',jobschema);
