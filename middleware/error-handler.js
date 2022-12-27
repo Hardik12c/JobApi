e721c111f052a26e3de19238ec76c62f187a8501
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-12-27 22:21:11 +0530

    cast error fixed

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,14 +4,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     status:err.statusCode ||StatusCodes.INTERNAL_SERVER_ERROR,
     messg:err.message||"Something went wrong Please try again"
   }
-  if(err.name==="ValidationError"){
-    customerror.messg=((Object.values(err.errors)).map((e)=>e.message)).join(',')
+  if(err.name==="ValidationError"){     
+    customerror.messg=((Object.values(err.errors)).map((e)=>e.message)).join(',');
     customerror.status=400;
+  }
+  if(err.name==="CastError"){     
+    customerror.messg=`No item found with id : ${err.value}`;
+    customerror.status=404;
   }
   if(err.code && err.code==11000){
     customerror.messg=`Duplicate value entered for ${Object.keys(err.keyValue)} Please enter a fresh value`;
     customerror.status=400;
   }
+  // return res.status(customerror.status).json({err});
   return res.status(customerror.status).json({messg:customerror.messg});
 }
 
