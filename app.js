f83bff5f7399988b1593e88139a81f5a1a279d9e
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-12-27 23:26:23 +0530

    app completedwith security added

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+// security packages
+const helmet=require('helmet');
+const cors=require('cors');
+const xss=require('xss-clean');
+const ratelimiter=require('express-rate-limit');
 // connectdb
 const connectDB=require('./db/connect');
 const authenticateuser=require('./middleware/authentication');
@@ -15,8 +19,21 @@ const jobrouter=require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy',1);
+app.use(ratelimiter({
+  windowMs:15*60*1000,
+  max:100,
+}));
+
+
 app.use(express.json());
 
+
+// using security packages
+
+app.use(helmet());
+app.use(cors());
+app.use(xss()); 
 // routes
 app.use('/api/v1/auth',authrouter);
 
